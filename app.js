c695bab389ded91bfa1d18c8c1657b8e25a3052e
Luis Henrique Ries <prof.ries@gmail.com>
2022-08-18 21:17:13 -0300

    Exercicio 1

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/produtos', (req, res) => {
+    res.send('Listando produtos!')
+})
+  
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
