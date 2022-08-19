5fd942ee6752e65987b8137d280976f5b04c8e6a
Luis Henrique Ries <prof.ries@gmail.com>
2022-08-18 21:29:57 -0300

    Exercicio 2

--- app.js
+++ app.js
@@ -2,14 +2,25 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
 app.get('/produtos', (req, res) => {
     res.send('Listando produtos!')
 })
+
+app.post('/produtos', (req, res) => {
+    res.send('Cadastrando Produtos!')
+})
+
+app.get('/produtos/:id', (req, res) => {
+    res.send('Buscando o produto '+req.params.id)
+})
+
+app.put('/produtos/:id', (req, res) => {
+    res.send('Atualizando o produto '+req.params.id)
+})
   
+app.delete('/produtos/:id', (req, res) => {
+    res.send('Removendo o produto '+req.params.id)
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
